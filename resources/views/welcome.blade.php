7f8f3c70fc1096194126a538e74fc36b20432452
knovator-7 <prashantmorem@knovator.in>
2019-05-15 15:24:57 +0530

    project configured

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Textile
                 </div>
 
                 <div class="links">
