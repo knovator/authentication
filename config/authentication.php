c83c96d27f130e3fffd577f821750bbd28d3e253
knovator-7 <prashantmorem@knovator.in>
2019-05-21 16:24:20 +0530

    package configured

---
+++ config/authentication.php
@@ -0,0 +1,54 @@
+<?php
+
+use Knovators\Authentication\Models\Permission;
+use Knovators\Authentication\Models\Role;
+use Knovators\Authentication\Models\User;
+use Knovators\Authentication\Http\Resources\User as UserResource;
+
+return [
+
+    'front_url' => env('APP_URL'),
+
+    'models' => [
+
+        'user' => User::class,
+
+        'role' => Role::class,
+
+        'permission' => Permission::class
+
+    ],
+
+    'resources' => [
+
+        'user' => UserResource::class,
+
+    ],
+
+    'permission' => [
+
+        'except_modules' => ['log-viewer', 'passport', 'auth']
+    ],
+
+    'login_columns' => 'email,phone',
+
+    'route' => [
+
+        'auth_attributes' => [
+
+            'prefix' => 'api/v1/auth',
+
+            'middleware' => env('AUTH_MIDDLEWARE') ? explode(',',
+                env('AUTH_MIDDLEWARE')) : [],
+        ],
+
+        'log_out_attributes' => [
+
+            'prefix' => 'api/v1/auth',
+
+            'middleware' => env('LOG_OUT_MIDDLEWARE') ? explode(',',
+                env('LOG_OUT_MIDDLEWARE')) : ['api'],
+        ]
+    ],
+
+];
