dcbb969ae62db4f9afebdb8de239d1d272095646
knovator-7 <prashantmorem@knovator.in>
2019-05-30 14:35:35 +0530

    state seeder created

--- app/Modules/User/Http/Resources/User.php
+++ app/Modules/User/Http/Resources/User.php
@@ -23,6 +23,7 @@ class User extends JsonResource
     public function toArray($request) {
         return [
             'id'         => $this->id,
+            'full_name'  => $this->full_name,
             'first_name' => $this->first_name,
             'last_name'  => $this->last_name,
             'email'      => $this->email,
