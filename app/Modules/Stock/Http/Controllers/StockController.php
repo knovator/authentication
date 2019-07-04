b9e2ea0c7fb832ae34fee77da656eb3be2cfc24f
Prashant Morem <prashantmorem@knovator.in>
2019-07-04 12:56:55 +0530

    api routes changed

---
+++ app/Modules/Stock/Http/Controllers/StockController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Modules\Stock\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class DeliveryController
+ * @package App\Modules\Stock\Http\Controllers
+ */
+class StockController extends Controller
+{
+
+
+
+
+
+
+
+}
