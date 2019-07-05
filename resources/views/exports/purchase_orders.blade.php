e1f87ad0320c74bab6ed6f2555f5d4d182b3b8f8
Prashant Morem <prashantmorem@knovator.in>
2019-07-05 18:05:40 +0530

    design export files converted into blade files

--- resources/views/exports/purchase_orders.blade.php
+++ resources/views/exports/purchase_orders.blade.php
@@ -2,27 +2,36 @@
     <thead>
     <tr>
         <th>Sr No</th>
-        <th>Customer Details</th>
         <th>Order No</th>
+        <th>Order Date</th>
+        <th>Customer Details</th>
+        <th>Threads</th>
+        <th>Status</th>
     </tr>
     </thead>
     <tbody>
     @foreach($orders as $orderKey => $order)
         <tr>
             <td rowspan="4">{{$orderKey + 1}}</td>
-            <td>Name : Prashantkumar Morem</td>
+            <td rowspan="4">{{$order->order_no}}</td>
+            <td rowspan="4">{{\Carbon\Carbon::parse($order->order_date)->format('D m Y')}}</td>
+            <td>{{$order->customer->full_name}}</td>
+            <td rowspan="4">@foreach($order->threads as $orderThread){{$orderThread->thread_color->thread->name}}
+                ({{$orderThread->kg_qty}})@if(!$loop->last){{','}}@endif @endforeach
+
+            </td>
+            <td rowspan="4">{{$order->status->name}}</td>
         </tr>
         <tr>
-            <td>Email1 : 9898870303</td>
+            <td>Phone: {{$order->customer->phone}}</td>
         </tr>
         <tr>
-            <td>Email2 : 9898870303</td>
+            <td>GST No: {{$order->customer->gst_no}}</td>
         </tr>
         <tr>
-            <td>Email3 : 9898870303</td>
+            <td>{{$order->customer->address.','.$order->customer->city_name}}</td>
         </tr>
-        <tr>
-            <td>Email3 : 9898870303</td>
+
     @endforeach
     </tbody>
 </table>
