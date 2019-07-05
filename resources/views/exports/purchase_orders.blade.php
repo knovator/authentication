cbf2ee03cd3c10c87c4f74ca56022b0781a4e35c
Prashant Morem <prashantmorem@knovator.in>
2019-07-05 16:59:58 +0530

    design export files converted into blade files

--- resources/views/exports/purchase_orders.blade.php
+++ resources/views/exports/purchase_orders.blade.php
@@ -2,16 +2,27 @@
     <thead>
     <tr>
         <th>Sr No</th>
-        <th>Customer Name</th>
+        <th>Customer Details</th>
+        <th>Order No</th>
     </tr>
     </thead>
-
     <tbody>
     @foreach($orders as $orderKey => $order)
         <tr>
-            <td>{{$orderKey + 1}}</td>
-            <td>{{$order->customer->full_name}}</td>
+            <td rowspan="4">{{$orderKey + 1}}</td>
+            <td>Name : Prashantkumar Morem</td>
         </tr>
+        <tr>
+            <td>Email1 : 9898870303</td>
+        </tr>
+        <tr>
+            <td>Email2 : 9898870303</td>
+        </tr>
+        <tr>
+            <td>Email3 : 9898870303</td>
+        </tr>
+        <tr>
+            <td>Email3 : 9898870303</td>
     @endforeach
     </tbody>
 </table>
