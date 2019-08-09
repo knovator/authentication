b8c24d0e17c1127f62a63d3a00e110eab8b1d82f
Prashant Morem <prashantmorem@knovator.in>
2019-08-09 12:23:48 +0530

    design list count issue solved

--- resources/views/exports/sales_orders.blade.php
+++ resources/views/exports/sales_orders.blade.php
@@ -2,6 +2,7 @@
     <thead>
     <tr>
         <th>Sr No</th>
+        <th>Order No</th>
         <th>Customer Name</th>
         <th>Order Date</th>
         <th>Design</th>
@@ -14,12 +15,13 @@
 
         @if($order->customer)
             <tr>
-                <td rowspan="4">{{$orderKey + 1}}</td>
-                <td>{{$order->customer->full_name}}</td>
-                <td rowspan="4">{{\Carbon\Carbon::parse($order->order_date)->format('D m Y')}}</td>
+                <td rowspan="5">{{$orderKey + 1}}</td>
+                <td rowspan="5">{{$order->order_no}}</td>
+                <td rowspan="5">{{$order->customer->full_name}}</td>
+                <td rowspan="5">{{\Carbon\Carbon::parse($order->order_date)->format('D m Y')}}</td>
+                <td rowspan="5">{{$order->design->quality_name}}</td>
                 <td>Total Mtr : {{ (!is_null($order->recipe_meters)) ? $order->recipe_meters->total:0 }}</td>
-                <td rowspan="4">{{$order->design->quality_name}}</td>
-                <td rowspan="4">{{$order->status->name}}</td>
+                <td rowspan="5">{{$order->status->name}}</td>
             </tr>
             <tr>
                 <td>Pending Mtr : {{ (!is_null($order->pending_meters)) ? $order->pending_meters:0 }}</td>
