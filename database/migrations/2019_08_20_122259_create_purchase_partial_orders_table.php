9ebc8a0ae0d1f15ef57cee42f8340cf176f4774f
Prashant Morem <prashantmorem@knovator.in>
2019-08-23 10:54:06 +0530

    migrations files removed

---
+++ database/migrations/2019_08_20_122259_create_purchase_partial_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreatePurchasePartialOrdersTable
+ */
+class CreatePurchasePartialOrdersTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('purchase_partial_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('purchase_order_thread_id');
+            $table->decimal('kg_qty');
+            $table->unsignedBigInteger('delivery_id');
+        });
+        Schema::table('purchase_partial_orders', function (Blueprint $table) {
+            $table->foreign('delivery_id')->references('id')->on('purchase_deliveries');
+            $table->foreign('purchase_order_thread_id')->references('id')
+                  ->on('purchase_order_threads');
+        });
+        Schema::table('purchase_deliveries', function (Blueprint $table) {
+            $table->decimal('total_kg')->after('bill_no');
+            $table->dropSoftDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('purchase_partial_orders');
+
+        Schema::table('purchase_deliveries', function (Blueprint $table) {
+            $table->dropColumn('kg_qty');
+            $table->softDeletes();
+        });
+    }
+}
