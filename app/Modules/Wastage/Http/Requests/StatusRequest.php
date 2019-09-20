050302ed54eb0c15ac23bf5f26043472db4298eb
Prashant Morem <prashantmorem@knovator.in>
2019-09-20 12:27:15 +0530

    machine assign api created

--- app/Modules/Wastage/Http/Requests/StatusRequest.php
+++ app/Modules/Wastage/Http/Requests/StatusRequest.php
@@ -45,13 +45,13 @@ class StatusRequest extends FormRequest
             case MasterConstant::WASTAGE_DELIVERED:
                 $currentStatusId = $this->retrieveMasterId(MasterConstant::WASTAGE_PENDING);
                 $validation = [
+                    'challan_no'               => 'required|string',
+                    'customer_id'              => 'required|exists:customers,id,deleted_at,NULL',
+                    'customer_po_number'       => 'nullable|string',
+                    'cost_per_meter'           => 'required|numeric',
+                    'manufacturing_company_id' => 'required',
                     'order_date'               => 'required|date_format:Y-m-d',
                     'delivery_date'            => 'required|date_format:Y-m-d|after_or_equal:order_date',
-                    'cost_per_meter'           => 'required|numeric',
-                    'customer_po_number'       => 'nullable|string',
-                    'customer_id'              => 'required|exists:customers,id,deleted_at,NULL',
-                    'challan_no'               => 'required|string',
-                    'manufacturing_company_id' => 'required',
                 ];
 
                 return array_merge($validation, $this->customValidation($currentStatusId));
