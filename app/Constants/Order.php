7fdee56270298a584065155dd7d6ba0cbac29643
Prashant Morem <prashantmorem@knovator.in>
2019-09-23 16:17:11 +0530

    changed order beam in machine invoice

--- app/Constants/Order.php
+++ app/Constants/Order.php
@@ -13,6 +13,7 @@ class Order
 
     // Order Types
     const FABRIC_ORDER = 'fabric';
+    const SALES_ORDER = 'sales';
     const YARN_ORDER = 'yarn';
     const WASTAGE_ORDER = 'wastage';
     const PURCHASE_ORDER = 'purchase';
--- app/Modules/Dashboard/Http/Controllers/DashboardController.php
+++ app/Modules/Dashboard/Http/Controllers/DashboardController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Modules\Dashboard\Http\Requests\AnalysisRequest;
 use App\Modules\Purchase\Repositories\PurchaseOrderRepository;
 use App\Modules\Sales\Repositories\SalesOrderRepository;
+use App\Modules\Stock\Repositories\StockRepository;
 use App\Modules\Wastage\Repositories\WastageOrderRepository;
 use App\Modules\Yarn\Repositories\YarnOrderRepository;
 use App\Repositories\MasterRepository;
@@ -36,6 +37,8 @@ class DashboardController extends Controller
 
     protected $purchaseOrderRepository;
 
+    protected $stockRepository;
+
     protected $masterRepository;
 
 
@@ -45,6 +48,7 @@ class DashboardController extends Controller
      * @param YarnOrderRepository     $yarnOrderRepository
      * @param WastageOrderRepository  $wastageOrderRepository
      * @param PurchaseOrderRepository $purchaseOrderRepository
+     * @param StockRepository         $stockRepository
      * @param MasterRepository        $masterRepository
      */
     public function __construct(
@@ -52,12 +56,14 @@ class DashboardController extends Controller
         YarnOrderRepository $yarnOrderRepository,
         WastageOrderRepository $wastageOrderRepository,
         PurchaseOrderRepository $purchaseOrderRepository,
+        StockRepository $stockRepository,
         MasterRepository $masterRepository
     ) {
         $this->salesOrderRepository = $salesOrderRepository;
         $this->yarnOrderRepository = $yarnOrderRepository;
         $this->wastageOrderRepository = $wastageOrderRepository;
         $this->purchaseOrderRepository = $purchaseOrderRepository;
+        $this->stockRepository = $stockRepository;
         $this->masterRepository = $masterRepository;
     }
 
@@ -86,34 +92,37 @@ class DashboardController extends Controller
     private function orderAnalysis($input) {
         $orders = [];
         $statuses = $this->orderStatuses();
-        if (in_array($orderType = OrderConstant::FABRIC_ORDER, $input['types'])) {
-            $orders[$orderType] = $this->commonOrderReport($input, 'salesOrderRepository', [
-                MasterConstant::SO_PENDING,
-                MasterConstant::SO_MANUFACTURING,
-                MasterConstant::SO_DELIVERED,
-            ], $statuses);
+
+        if (in_array($orderType = OrderConstant::SALES_ORDER, $input['types'])) {
+            $orders[$orderType] = $this->commonOrderReport($input, OrderConstant::SALES_ORDER,
+                'salesOrderRepository', [
+                    MasterConstant::SO_PENDING,
+                    MasterConstant::SO_MANUFACTURING,
+                    MasterConstant::SO_DELIVERED,
+                ], $statuses);
         }
         if (in_array($orderType = OrderConstant::YARN_ORDER, $input['types'])) {
-            $orders[$orderType] = $this->commonOrderReport($input, 'yarnOrderRepository', [
-                MasterConstant::SO_PENDING,
-                MasterConstant::SO_DELIVERED,
-            ], $statuses);
+            $orders[$orderType] = $this->commonOrderReport($input, OrderConstant::YARN_ORDER,
+                'yarnOrderRepository', [
+                    MasterConstant::SO_PENDING,
+                    MasterConstant::SO_DELIVERED,
+                ], $statuses);
         }
-
 
         if (in_array($orderType = OrderConstant::WASTAGE_ORDER, $input['types'])) {
-            $orders[$orderType] = $this->commonOrderReport($input, 'wastageOrderRepository', [
-                MasterConstant::WASTAGE_PENDING,
-                MasterConstant::WASTAGE_DELIVERED,
-            ], $statuses);
+            $orders[$orderType] = $this->commonOrderReport($input, OrderConstant::WASTAGE_ORDER,
+                'wastageOrderRepository', [
+                    MasterConstant::WASTAGE_PENDING,
+                    MasterConstant::WASTAGE_DELIVERED,
+                ], $statuses);
         }
 
-
         if (in_array($orderType = OrderConstant::PURCHASE_ORDER, $input['types'])) {
-            $orders[$orderType] = $this->commonOrderReport($input, 'purchaseOrderRepository', [
-                MasterConstant::PO_PENDING,
-                MasterConstant::PO_DELIVERED,
-            ], $statuses);
+            $orders[$orderType] = $this->commonOrderReport($input, OrderConstant::PURCHASE_ORDER,
+                'purchaseOrderRepository', [
+                    MasterConstant::PO_PENDING,
+                    MasterConstant::PO_DELIVERED,
+                ], $statuses);
         }
 
         return $orders;
@@ -140,21 +149,25 @@ class DashboardController extends Controller
 
     /**
      * @param                      $input
+     * @param                      $type
      * @param string               $repository
      * @param                      $statusCodes
-     * @param                      $statuses
+     * @param                      $allStatuses
      * @return mixed
      */
-    private function commonOrderReport($input, $repository, $statusCodes, $statuses) {
-        /** @var \Illuminate\Support\Collection $statuses */
-        $statusIds = $statuses->whereIn('code', $statusCodes)->pluck('id')->toArray();
-        $totalOrders = $this->{$repository}->getOrderAnalysis($input, $statusIds);
+    private function commonOrderReport($input, $type, $repository, $statusCodes, $allStatuses) {
+        /** @var \Illuminate\Support\Collection $allStatuses */
+        $statuses = $allStatuses->whereIn('code', $statusCodes)->all();
+        $stock = $this->stockRepository->getOrderAnalysisReport($input, $type, $statuses);
+        $totalOrders = $this->{$repository}->getOrderAnalysis($input,
+            array_column($statuses, 'id'));
         /** @var Collection $totalOrders */
-        $orders['count']['total'] = $totalOrders->sum('total');
+        $orders['order']['total'] = $totalOrders->sum('total');
         foreach ($statusCodes as $statusCode) {
-            $orders['count'][strtolower($statusCode)] = isset($totalOrders[$statuses[$statusCode]['id']]) ?
+            $orders['order'][strtolower($statusCode)] = isset($totalOrders[$statuses[$statusCode]['id']]) ?
                 $totalOrders[$statuses[$statusCode]['id']]['total'] : 0;
         }
+        $orders['meters'] = $stock;
 
         return $orders;
     }
