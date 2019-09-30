3d925c6fc57039fa6f40487153518bde3a7dbd04
Prashant Morem <prashantmorem@knovator.in>
2019-09-30 18:16:35 +0530

    purchase order list order changed

--- app/Modules/Sales/Support/CommonReportService.php
+++ app/Modules/Sales/Support/CommonReportService.php
@@ -30,51 +30,39 @@ trait CommonReportService
                               ->groupBy('year');
 
         if (isset($input['date_range'])) {
-            $orders = $orders->whereDate('order_date', '>=', $input['date_range']['start_date'])
-                             ->whereDate('order_date', '<=', $input['date_range']['end_date']);
+            $orders = $orders->whereDate('order_date', '>=', $from)
+                             ->whereDate('order_date', '<=', $to);
         }
 
 
         switch ($input['group']) {
 
             case 'daily':
-                $orders = $orders->selectRaw('order_date as start_date,order_date as end_date')
+                $orders = $orders->selectRaw('order_date as end_date')
                                  ->groupBy('order_date')->get()->keyBy('end_date');
 
                 return $this->generateDailyDateRange($orders, $from, $to, $field);
 
             case 'monthly':
-//                $orders = $orders->selectRaw('MONTH(order_date) as month,DATE_SUB(LAST_DAY(order_date),INTERVAL DAY(LAST_DAY(order_date))- 1 DAY) AS start_date,LAST_DAY(order_date) AS end_date')
-//                                 ->groupBy('month')->get();
                 $orders = $orders->selectRaw('MONTH(order_date) as month')
                                  ->groupBy('month')->get()->keyBy('month');
 
-                return $this->generateMonthDateRange($orders, $from, $to, $field);
-                break;
+                return $this->generateDateRange($orders, $from, $to, $field, 'month');
 
             case 'weekly':
-                $orders = $orders->selectRaw('WEEK(order_date) as week,DATE(order_date + INTERVAL ( - WEEKDAY(order_date)) DAY) as start_date,DATE(order_date + INTERVAL (6 - WEEKDAY(order_date)) DAY) as end_date')
-                                 ->groupBy('week');
+                $orders = $orders->selectRaw('WEEK(order_date) as week')
+                                 ->groupBy('week')->get()->keyBy('week');
+
+                return $this->generateDateRange($orders, $from, $to, $field, 'week');
                 break;
 
-            case 'yearly':
-                $orders = $orders->selectRaw('DATE_FORMAT(order_date ,"%Y-01-01") as start_date,DATE_FORMAT(order_date ,"%Y-12-31") as end_date');
-                break;
+            default:
+                return $this->generateDateRange($orders->get()->keyBy('year'), $from, $to,
+                    $field, 'year');
         }
 
-        return $orders;
+
     }
-
-
-//    private function generateDateRange(Carbon $start_date, Carbon $end_date) {
-//        $dates = [];
-//        for ($date = $start_date->startOfYear(); $date->lte($end_date); $date->addYear()) {
-//            $dates[] = $date->format('Y-m-d');
-//        }
-//
-//        return $dates;
-//
-//    }
 
     /**
      * @param        $orders
@@ -85,8 +73,6 @@ trait CommonReportService
      */
     private function generateDailyDateRange($orders, Carbon $from, Carbon $to, $quantityType) {
         $dates = [];
-
-
         for ($date = $from; $date->lte($to); $date->addDay()) {
             $newDate = $date->format('Y-m-d');
             if (isset($orders[$newDate])) {
@@ -135,43 +121,39 @@ trait CommonReportService
      * @param Carbon $from
      * @param Carbon $to
      * @param        $quantityType
+     * @param        $type
      * @return array
      */
-    private function generateMonthDateRange($orders, Carbon $from, Carbon $to, $quantityType) {
+    private function generateDateRange(
+        $orders,
+        Carbon $from,
+        Carbon $to,
+        $quantityType,
+        $type
+    ) {
 
-        $months = [];
-        for ($month = $from; $month->lte($to); $month->startOfMonth()->addMonth()) {
-            $monthInt = $month->month;
+        $dates = [];
+        for ($date = $from; $date->lte($to); $from->{'startOf' . $type}()->{'add' . $type}()) {
+            $dateInt = $date->{$type};
+            $startDate = $date->format('Y-m-d');
+            $endDate = $date->endOfMonth();
 
-            $startDate = $month->format('Y-m-d');
-            $endDate = $month->endOfMonth();
-
-            if ($month->endOfMonth()->gte($to)) {
+            if ($endDate->gt($to)) {
                 $endDate = $to;
             }
 
-            if (isset($orders[$monthInt])) {
-                $order = $orders[$monthInt];
-                $months[] = $this->formatDateResponse($startDate,
+            if (isset($orders[$dateInt])) {
+                $order = $orders[$dateInt];
+                $dates[] = $this->formatDateResponse($startDate,
                     $endDate->format('Y-m-d'),
                     $order->total_orders, $order->{$quantityType}, $quantityType);
             } else {
-                $months[] = $this->formatDateResponse($startDate,
+                $dates[] = $this->formatDateResponse($startDate,
                     $endDate->format('Y-m-d'),
                     0, 0, $quantityType);
             }
         }
 
-        return $months;
+        return $dates;
     }
-
-
-//    private function generateDateRange(Carbon $start_date, Carbon $end_date) {
-//        $startDate = Carbon::parse($start_date)->subDays($start_date->weekday());
-//        $dates = [];
-//        for ($date = $startDate; $date->lte($end_date->endOfWeek()); $date->addWeek()) {
-//            $dates[] = $date->format('Y-m-d');
-//        }
-//        return $dates;
-//    }
 }
